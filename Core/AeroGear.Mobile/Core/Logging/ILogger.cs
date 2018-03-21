1911bde119a15788a3406e605463d590bfc8c225
Vojtech Sazel <vsazel@gmail.com>
2018-03-21 16:29:27 +0100

    added ILogger interface for logging

--- Core/AeroGear.Mobile/Core/Logging/ILogger.cs
+++ Core/AeroGear.Mobile/Core/Logging/ILogger.cs
@@ -1,7 +1,43 @@
 ﻿using System;
+using SystemException=System.Exception;
+
 namespace Core.AeroGear.Mobile.Core.Logging
 {
+    /// <summary>
+    /// Common interface for logging.
+    /// </summary>
     public interface ILogger
     {
+        void Info(String tag, String message);
+
+        void Info(String message);
+
+        void Info(String tag, String message, SystemException e);
+
+        void Info(String message, SystemException e);
+
+        void Warning(String tag, String message);
+
+        void Warning(String message);
+
+        void Warning(String tag, String message, SystemException e);
+
+        void Warning(String message, SystemException e);
+
+        void Debug(String tag, String message);
+
+        void Debug(String message);
+
+        void Debug(String tag, String message, SystemException e);
+
+        void Debug(String message, SystemException e);
+
+        void Error(String tag, String message);
+
+        void Error(String message);
+
+        void Error(String tag, String message, SystemException e);
+
+        void Error(String message, SystemException e);
     }
 }
