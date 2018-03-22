1066f924b3c6448ab543b9d03a83d35722a126b8
Massimiliano Ziccardi <mziccard@redhat.com>
2018-03-22 15:25:17 +0100

    Added auth page

---
+++ Example/Example/Views/Pages/AuthPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+using Xamarin.Forms.Xaml;
+namespace Example.Views.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AuthPage : ContentPage
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
