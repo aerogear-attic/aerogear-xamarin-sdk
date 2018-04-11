70ec8b20a6dcb2b3dcb8a6cfabfd3fe7c6687a95
rachael-oregan <roregan@redhat.com>
2018-04-11 11:52:10 +0100

    unit tests

--- Auth/Config/AuthenticationConfig.cs
+++ Auth/Config/AuthenticationConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
 
-namespace Core.AeroGear.Mobile.Auth.Config
+namespace Auth.Config
 {
     /// <summary>
     /// Configurations for the authentication service
@@ -11,8 +11,24 @@ namespace Core.AeroGear.Mobile.Auth.Config
         /// <returns>the redirect uri used during the authentication process</returns>
         public Uri RedirectUri { get; private set; }
 
-        /// <returns>the OIDC scopes for the auth request. If no scopes are defined, the default 'openid scope will be sent</returns>
-        public string Scopes { get; private set; }
+        private string _Scopes;
+
+        private const string defaultScope = "openid";
+
+        /// <returns>the OIDC scopes for the auth request. If no scopes are defined, the default 'openid' scope will be sent</returns>
+        public string Scopes 
+        { 
+            get {
+                return _Scopes;
+            } 
+            private set {
+                if (value != null) {
+                    _Scopes = value;
+                } else {
+                    _Scopes = defaultScope;
+                }
+            } 
+        }
 
         /// <returns>the minimum time between Json Web Key Set (JWKS) requests in minutes. Default value is 1440 (1 day)</returns>
         public ushort MinTimeBetweenJwksRequests { get; private set; }
@@ -40,16 +56,16 @@ namespace Core.AeroGear.Mobile.Auth.Config
         public class AuthenticationConfigBuilder
         {
             private Uri redirectUri;
-            private string scopes = "openid";
+            private string scopes;
             private ushort minTimeBetweenJwksRequests = 24 * 60;
 
             /// <summary>Specify the redirect value</summary>
             /// <param name="redirectUri">redirect uri value</param>
             /// <returns>AuthenticationConfigBuilder instance</returns>
-            public AuthenticationConfigBuilder RedirectUri(Uri redirectUri)
+            public AuthenticationConfigBuilder RedirectUri(string redirectUri)
             {
-                Contract.Requires(redirectUri != null);
-                this.redirectUri = redirectUri;
+                Contract.Requires(!string.IsNullOrEmpty(redirectUri));
+                this.redirectUri = new Uri(redirectUri);
                 return this;
             }
 
