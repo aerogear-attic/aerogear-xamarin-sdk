6e51efd406ce88b050a069604f5b1b053a5393f9
Vojtěch Sázel <vojtech@sazel.cz>
2018-04-16 10:36:30 +0200

    fixes for SanityChecks after folder restructure

---
+++ Core/Core.Tests/Utils/SanityCheckTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AeroGear.Mobile.Core.Utils
+{
+    [TestFixture]
+    public class SanityCheckTests
+    {
+        [Test]
+        public void testNonNull()
+        {
+            try
+            {
+                SanityCheck.nonNull((String)null, "test-param");
+                Assert.Fail("null value has not been detected");
+            }
+            catch (ArgumentNullException ane)
+            {
+                Assert.AreEqual(String.Format("Value cannot be null.{0}Parameter name: test-param", Environment.NewLine), ane.Message);
+            }
+        }
+
+        [Test]
+        public void testNonEmpty()
+        {
+            try
+            {
+                SanityCheck.nonEmpty("     ", "empty-string");
+                Assert.Fail("empty value has not been detected");
+            }
+            catch (ArgumentException ae)
+            {
+                Assert.AreEqual("'empty-string' can't be empty or null", ae.Message);
+            }
+        }
+
+        [Test]
+        public void testNonEmptyNoTrim()
+        {
+            SanityCheck.nonEmpty("     ", "empty-string", false);
+        }
+
+        [Test]
+        public void testNonEmptyWithCustomMessage()
+        {
+            try
+            {
+                SanityCheck.nonEmpty("     ",
+                                     "Parameter '{0}' must be valorised and only spaces are not accepted",
+                                     "testParam");
+                Assert.Fail("empty value has not been detected");
+            }
+            catch (ArgumentException ae)
+            {
+                Assert.AreEqual(
+                    "Parameter 'testParam' must be valorised and only spaces are not accepted",
+                    ae.Message);
+            }
+        }
+    }
+}
