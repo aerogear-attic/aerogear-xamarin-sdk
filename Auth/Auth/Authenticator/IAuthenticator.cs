dc03d47dd2e8606d0e34c10c353d079ef8c84393
Wei Li <wei.li@redhat.com>
2018-04-18 22:03:13 +0100

    ✨ add the authenticator interface, and android implementation

---
+++ Auth/Auth/Authenticator/IAuthenticator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Aerogear.Mobile.Auth.User;
+
+namespace AeroGear.Mobile.Auth.Authenticator
+{
+    public interface IAuthenticator
+    {
+        Task<User> Authenticate(IAuthenticateOptions authenticateOptions);
+
+        Task<bool> Logout(User currentUser);
+    }
+}
