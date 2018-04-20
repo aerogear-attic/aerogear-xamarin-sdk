8a2d33a68390f5edcadbc40f96f241c4fb5257fe
Massimiliano Ziccardi <mziccard@redhat.com>
2018-04-20 18:34:22 +0200

    Added service finder

---
+++ Auth/Auth.Platform.iOS/AuthService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AeroGear.Mobile.Core;
+using AeroGear.Mobile.Core.Configuration;
+
+namespace AeroGear.Mobile.Auth
+{
+    public class AuthService : IServiceModule
+    {
+        public AuthService()
+        {
+        }
+
+        public string Type => throw new NotImplementedException();
+
+        public bool RequiresConfiguration => throw new NotImplementedException();
+
+        public void Configure(MobileCore core, ServiceConfiguration serviceConfiguration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Destroy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
