2a57f2d32c7852854ee351bd19ecec6fefe1b5f7
Hoyt Summers Pittman <secondsun@gmail.com>
2018-04-25 23:22:37 -0400

    ServiceRegistry hooks WIP

---
+++ Core/Core/Utils/ServiceAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AeroGear.Mobile.Core.Utils
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    ///<summary>
+    ///This Attribute is used to inform <see cref="MobileCore"/> of implementations of Services.  This is used to provide the correct concrete implementation
+    ///for a platform.
+    ///</summary>
+    public class ServiceAttribute : Attribute
+    {
+        public ServiceAttribute(Type implementorType)
+        {
+            Implementor = implementorType;
+        }
+
+        internal Type Implementor { get; private set; }
+    }
+}
