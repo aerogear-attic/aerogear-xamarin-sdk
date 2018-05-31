c8d2cee35195114d367177be46c84b0b71076485
Massimiliano Ziccardi <mziccard@redhat.com>
2018-05-31 12:22:31 +0200

    Added some documentation

--- Core/Core/Utils/PlatformBridge/AbstractUserPreferences.cs
+++ Core/Core/Utils/PlatformBridge/AbstractUserPreferences.cs
@@ -2,10 +2,30 @@
 using static AeroGear.Mobile.Core.Utils.SanityCheck;
 namespace AeroGear.Mobile.Core.Utils
 {
+    /// <summary>
+    /// Base class for user preferences. Automatically performs sanity checks on parameters.
+    /// </summary>
     public abstract class AbstractUserPreferences : IUserPreferences
     {
+        /// <summary>
+        /// Returns the string associated with the given key.
+        /// </summary>
+        /// <returns>The get string.</returns>
+        /// <param name="key">Key.</param>
+        /// <param name="defaultValue">Default value.</param>
         protected abstract string doGetString(string key, string defaultValue = null);
+
+        /// <summary>
+        /// Adds a new key,value pair to the user preferences.
+        /// </summary>
+        /// <param name="key">Key.</param>
+        /// <param name="value">Value.</param>
         protected abstract void doPutString(string key, string value);
+
+        /// <summary>
+        /// Remove the value associated to the specific key from the user preferences.
+        /// </summary>
+        /// <param name="key">Key.</param>
         protected abstract void doRemoveValue(string key);
 
         public string GetString(string key, string defaultValue = null)
--- Core/Core/Utils/PlatformBridge/PlatformInfo.cs
+++ Core/Core/Utils/PlatformBridge/PlatformInfo.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 namespace AeroGear.Mobile.Core.Utils
 {
+    /// <summary>
+    /// This class allows the shared code to have info of the real platform the app is running in.
+    /// </summary>
     public class PlatformInfo
     {
+        /// <summary>
+        /// Operating system name.
+        /// </summary>
         public readonly string Name;
+
+        /// <summary>
+        /// Operating system version.
+        /// </summary>
         public readonly string Version;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:AeroGear.Mobile.Core.Utils.PlatformInfo"/> class.
+        /// </summary>
+        /// <param name="name">Operating system name.</param>
+        /// <param name="version">Operating system version.</param>
         public PlatformInfo(string name, string version)
         {
             this.Name = name;
